f1351ebc89b73b1fa57acea606aa2869cc432115
Natalia Mirnaia <strunailustra@gmail.com>
2020-05-29 14:02:15 +0300

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Наталья Мирная on 29.05.2020.
+//  Copyright © 2020 Наталья Мирная. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
