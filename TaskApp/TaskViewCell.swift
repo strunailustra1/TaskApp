82eaac9f23abf454ddec7c677b64d95aff887573
Natalia Mirnaia <strunailustra@gmail.com>
2020-06-18 13:36:06 +0300

    tt

---
+++ TaskApp/TaskViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TaskViewCell.swift
+//  TaskApp
+//
+//  Created by Наталья Мирная on 01.06.2020.
+//  Copyright © 2020 Наталья Мирная. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewCell: UITableViewCell {
+
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var numberOfTasks: UILabel!
+    
+    @IBAction func doneAction(_ sender: UIButton) {
+    }
+    
+}
